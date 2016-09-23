0919ce802d5c5b8c43b4b7bc2c7a30cc7b993e2f
mahat <mehmet.ahat@gmail.com>
2016-09-23 04:28:46 +0300

    third commit with additional problem and pdf of problem

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ MPG of Japan's drivers and USA's drivers. There are 79 sample from Japan, 250 sa
 # Hypotheses
 - MPG means of USA and Japan are equal
 - USA's MPG mean is greater or equal than Japan's
+- A sample problem
 
 # Usage
 You can run program with TtestExample.py. Due to matplotlib, running can be suspended. You can change showPlots to False.
--- TtestExample.py
+++ TtestExample.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.stats import ttest_ind
+from scipy.special._ufuncs import stdtr
+from scipy.stats import ttest_ind, ttest_ind_from_stats
+from numpy import sqrt
 
 # read data
 data = pd.read_csv('./dataset/MPG_DataSet.csv', sep=';')
@@ -17,7 +19,7 @@ print'Japan data mean %f' % JapanMpg.mean()
 
 
 # plot the data
-showPlots = True
+showPlots = False
 if showPlots:
     fig = plt.figure('T-Test')
     fig.suptitle("Box Plots", fontsize=16)
@@ -52,7 +54,7 @@ It goes on to say that scipy always gives the test statistic as signed.
 This means that given p and t values from a two-tailed test,
 you would reject the null hypothesis of a greater-than test when p/2 < alpha and t > 0, and of a less-than test when p/2 < alpha and t < 0.
 
-Another good soruce: http://stattrek.com/hypothesis-test/difference-in-means.aspx?Tutorial=AP
+
 '''
 
 # One Tailed Hypothesis
@@ -70,3 +72,46 @@ else:
     print ('For H_3: Null hypothesis cannot be rejected!')
 
 print(' ------------- ')
+
+
+''' Another good soruce: http://stattrek.com/hypothesis-test/difference-in-means.aspx?Tutorial=AP '''
+# Problem 2: One-Tailed Test
+#
+#  The Acme Company has developed a new battery. The engineer in charge claims that the new battery will operate continuously for at least 7 minutes longer than the old battery.
+# To test the claim, the company selects a simple random sample of 100 new batteries and 100 old batteries. The old batteries run continuously for 190 minutes with a standard deviation of 20 minutes; the new batteries, 200 minutes with a standard deviation of 40 minutes.
+# Test the engineer's claim that the new batteries run at least 7 minutes longer than the old. Use a 0.05 level of significance. (Assume that there are no outliers in either sample.)
+# Solution: The solution to this problem takes four steps: (1) state the hypotheses, (2) formulate an analysis plan, (3) analyze sample data, and (4) interpret results. We work through those steps below:
+
+#Null hypothesis: Mu1 - Mu2 >= 7
+#Alternative hypothesis: Mu1 - Mu2 < 7
+
+
+na = float(100)
+adof = na - 1
+abar = float(200)
+avar = float(40)**2 * (na / adof)
+
+
+nb = float(100)
+bdof = nb - 1
+bbar = float(190)
+bvar = float(20)**2 * (nb / bdof)
+
+# Use scipy.stats.ttest_ind_from_stats.
+t2, p2 = ttest_ind_from_stats(abar, sqrt(avar), na,
+                              bbar, sqrt(bvar), nb,
+                              equal_var=False)
+print("ttest_ind_from_stats: t = %g  p = %g" % (t2, p2))
+
+# Use the formulas directly.
+tf = (abar - bbar - 7) / sqrt(avar/na + bvar/nb)
+dof = (avar/na + bvar/nb)**2 / (avar**2/(na**2*adof) + bvar**2/(nb**2*bdof))
+
+#calculating p value of P(T > tf)
+pval = stdtr(dof, abs(tf))
+
+print 't = %f p = %f dof = %f' % (tf,pval,dof)
+if pval < significance:
+    print ('It is unlikely to except null hypothesis!')
+else:
+    print ('Null hypothesis cannot be rejected!')
